c0ebf12abbeb9c1ebf66cad443c351b60fa8da42
EliW09 <43866694+EliW09@users.noreply.github.com>
2018-12-17 18:20:06 -0500

    Revert "Authpage"

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,11 @@
 'use strict'
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-const events = require('./events.js')
 
 $(() => {
-  $('#sign-up').on('submit', events.onSignUp)
-  $('#sign-in').on('submit', events.onSignIn)
-  $('#change-password').on('submit', events.changePassword)
-  $('#sign-out').on('submit', events.onSignOut)
-  $('#Add').on('submit', events.onCreate)
-  $('.Edit').on('submit', events.onUpdate)
-  $('.Delete').on('submit', events.onDelete)
-  $('.Index').on('submit', events.onIndex)
-  $(document).ready(events.carousel)
-  $('.message a').click(function () {
-    $('form').animate({height: 'toggle', opacity: 'toggle'}, 'slow')
-  })
+  // your JS code goes here
 })
